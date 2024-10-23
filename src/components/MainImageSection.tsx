2ff1e3026adb32c969f16d97d1b63baa8b0c3f8c
shakir <shakirm@carestack.com>
2024-10-23 19:45:09 +0530

    inside page hero changes

--- src/components/MainImageSection.tsx
+++ src/components/MainImageSection.tsx
@@ -32,35 +32,35 @@ const MainImageSection = ({ post, isAuthor, enableDate = false, isAudio = false 
 	}
 
 	return (
-		<div className='w-full flex gap-1 min-h-[423px] items-center bg-black relative '>
-			<Section className={`items-start`}>
-			<Wrapper className="z-10 flex h-auto">
-				<div className='flex flex-col items-start gap-32 bg-black text-white md:max-w-xl max-w-lg h-full justify-center pr-8'>
-					<div className='flex flex-col items-start '>
-						<Breadcrumb />
-						<h1 className="text-white font-manrope leading-tight md:text-5xl text-2xl font-bold  mb-[10px]">
-							{post.title ? post.title : 'Post Title'}
-						</h1>
-						<p className="text-gray-400 font-inter text-base font-normal leading-120 line-clamp-2 overflow-hidden">
-							{post.excerpt ? post.excerpt : 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur except.'}
-						</p>
-						{isAuthor && <AuthorInfo className={"mt-8"} contentType={post.contentType} author={post?.author} />}
-						{
-							enableDate && <DurationSection isAudio={isAudio} duration={post?.estimatedReadingTime ? post.estimatedReadingTime : post.duration} date={post?.date ? post?.date : ""} />
-						}
+		<div className='w-full flex gap-1 items-center bg-zinc-900 relative overflow-hidden'>
+			<Section className={`justify-center w-full `}>
+				<Wrapper className="z-10 flex h-auto">
+					<div className='flex flex-col items-start gap-32 text-white md:max-w-[46%] max-w-lg h-full justify-center py-8 md:py-24'>
+						<div className='flex flex-col items-start '>
+							<Breadcrumb />
+							<h1 className="text-white font-manrope leading-tight lg:text-5xl text-2xl font-bold  mb-[10px]">
+								{post.title ? post.title : 'Post Title'}
+							</h1>
+							<p className="text-gray-400 font-inter text-base font-normal leading-120 line-clamp-2 overflow-hidden">
+								{post.excerpt ? post.excerpt : 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur except.'}
+							</p>
+							{isAuthor && <AuthorInfo className={"mt-8"} contentType={post.contentType} author={post?.author} />}
+							{
+								enableDate && <DurationSection isAudio={isAudio} duration={post?.estimatedReadingTime ? post.estimatedReadingTime : post.duration} date={post?.date ? post?.date : ""} />
+							}
+						</div>
 					</div>
-				</div>
-			</Wrapper>
-			<div className='w-full md:flex hidden absolute h-full justify-end'>
-				<div className='md:w-1/2 w-1/3'>
-					<ImageLoader
-						image={post.mainImage || post.image}
-						priority={true}
-						alt={post.title || "Post image"}
-						client={client}
-					/>
-				</div>
-			</div>
+					<div className='absolute left-1/2 right-0 top-0 bottom-0 w-auto'>
+							<ImageLoader
+								image={post.mainImage || post.image}
+								priority={true}
+								useClientWidth={true}
+								alt={post.title || "Post image"}
+								client={client}
+								imageClassName='w-full h-full object-cover'
+							/>
+					</div>
+				</Wrapper>
 			</Section>
 		</div>
 	)
