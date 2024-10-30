3d52ae64632dacb868570e906ea86f5ea2f98c02
shakir <shakirm@carestack.com>
2024-10-30 15:54:52 +0530

    header and card fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,8 +11,7 @@ interface LayoutProps {
 
 export default function Layout({ children, className, fullWidth = false }: LayoutProps) {
   return (
-
-    <div className={`flex flex-col w-full items-center `}>
+    <div className={`flex flex-col w-full items-center pt-[110px] lg:pt-[138px]`}>
       <Header />
       <main className='w-full flex flex-col'>{children}</main>
       <Footer className={`w-full flex `} />
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import SearchBar from '~/components/widgets/SearchBar';
 import ProgressBar from '~/utils/progressBar/progressBar';
 import {ArrowRightIcon} from '@sanity/icons'
 import { useRouter } from 'next/router';
 import Logo from '~/assets/reactiveAssets/Logo';
+import {MenuIcon} from '@sanity/icons'
+import {CloseIcon} from '@sanity/icons'
+import useMediaQuery from '~/utils/useMediaQueryHook';
 
 const navigationLinks = [
   { href: "/case-study", label: "Case Studies" },
@@ -16,14 +19,40 @@ const navigationLinks = [
 ];
 
 const Header = () => {
+  
+  const [showMenu, setShowMenu] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
+
+  const closeMenu = () => {
+    setShowMenu(false);  
+  };
+
+  const toggleMenu = () => setShowMenu(!showMenu);
+
+  const handleScrollMob = () => {
+    setHeaderFixed(window.scrollY > 44);
+  };
+
+  const isMobile: any = useMediaQuery(1024);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollMob);
+    return () => {
+      window.removeEventListener("scroll", handleScrollMob);
+    }
+  });
 
   const router = useRouter();
   return (
     <>
     <ProgressBar/>
-    <header className="w-full">
+    <header className={`fixed w-full top-0 left-0 z-10 transition-all duration-300 ease-linear ${headerFixed && '!fixed w-full !top-[-44px] left-0'}`}>
 
-      <div className="bg-cs-primary group hover:bg-[#42dd88] transition-all duration-200 px-4">  
+      <div className={`bg-cs-primary group hover:bg-[#42dd88] transition-all duration-200 px-4 h-[44px]`}>  
         <Link href="https://carestack.com/demo" className="flex justify-center py-3">
           <div className="max-w-7xl flex justify-center gap-3 w-full items-center">
             <div className="text-xs md:text-sm text-zinc-900">
@@ -36,16 +65,16 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="bg-zinc-900 text-white border-b border-zinc-800 px-4">
+      <div className={`z-10 bg-zinc-900 text-white border-b border-zinc-800 px-4  `}>
         <div className="max-w-7xl mx-auto">
-          <div className="flex flex-col gap-3 justify-between py-6">
-            <div className='flex md:flex-row gap-2 flex-col justify-between'>
+          <div className="flex flex-col gap-3 justify-between py-[10px] lg:py-6">
+            <div className='flex flex-row gap-2 justify-between'>
               <Link href="/" className="text-2xl font-extrabold bg-gradient-text bg-clip-text text-transparent font-monrope tracking-tighterText">
-              <Logo/> 
+                <Logo/> 
               </Link>
-              
-              <div className='flex md:flex-row flex-col gap-2 justify-between items-center'>
-                <nav className="flex gap-y-2 gap-x-6 md:gap-x-10 flex-wrap">
+              <div onClick={toggleMenu} className='flex lg:hidden text-zinc-400'> {showMenu ? <CloseIcon width={40} height={40}/>: <MenuIcon width={40} height={40}/>}</div>
+              <div className={`lg:flex-row absolute top-[110px] right-0 z-10 px-4 pt-4 pb-8 lg:p-0 bg-zinc-900 lg:bg-transparent left-0 lg:static flex-col gap-2 justify-between lg:items-center lg:flex ${showMenu ? 'flex' : 'hidden'}`}>
+                <nav className="flex flex-col lg:flex-row gap-y-2 gap-x-6 lg:gap-x-10 flex-wrap">
                   {navigationLinks?.map((link) => (
                     
                     <Link 
