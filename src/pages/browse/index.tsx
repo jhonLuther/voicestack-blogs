d66290d2b98adb0c63b095f17706846e6ae305c6
Akash A S <akashas@carestack.com>
2024-11-02 13:06:27 +0000

    Merged in dev (pull request #68)
    Custom image missing issue resolved
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * detail page fix
    
    * mob fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * mob detail fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * custom meta tag fixes
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * custom meta tag fixes
    
    * header fix
    
    * Merge branch 'dev' of https://bitbuckett pull upstream main:wq.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * browse all page mob style
    
    * bug fix
    
    * author page
    
    * image loader issue fixed featured section can be now customised from homesettings
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * casestudy hero changed static
    
    * merge fix
    
    * site setting updated for meta tag
    
    * merge fix
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * cutom image bug fixes

--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -2,13 +2,14 @@ import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Layout from '~/components/Layout'
 import { getClient } from '~/lib/sanity.client'
 import {
-	getArticlesCount,
-	getEbooksCount,
-	getPodcastsCount,
-	getPosts,
-	getPostsByLimit,
-	getTags,
-	getWebinarsCount,
+  getArticlesCount,
+  getEbooksCount,
+  getPodcastsCount,
+  getPosts,
+  getPostsByLimit,
+  getSiteSettings,
+  getTags,
+  getWebinarsCount,
 } from '~/lib/sanity.queries'
 import AllcontentSection from '~/components/sections/AllcontentSection'
 import Pagination from '~/components/commonSections/Pagination'
@@ -19,94 +20,98 @@ import { useRef } from 'react'
 import router, { useRouter } from 'next/router'
 import ContentHub from '~/contentUtils/ContentHub'
 import { BaseUrlProvider } from '~/components/Context/UrlContext'
+import { defaultMetaTag } from '~/utils/customHead'
 
 interface Query {
-	[key: string]: string
+  [key: string]: string
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-	const client = getClient();
-	const pageNumber = params?.pageNumber ? parseInt(params.pageNumber as string, 10) : 1;
+  const client = getClient()
+  const pageNumber = params?.pageNumber
+    ? parseInt(params.pageNumber as string, 10)
+    : 1
 
-	const cardsPerPage = siteConfig.pagination.itemsPerPage || 5;
-	const startLimit = (pageNumber - 1) * cardsPerPage;
+  const cardsPerPage = siteConfig.pagination.itemsPerPage || 5
+  const startLimit = (pageNumber - 1) * cardsPerPage
 
-	const tags = await getTags(client);
+  const tags = await getTags(client)
 
-	const endLimit = startLimit + cardsPerPage;
+  const endLimit = startLimit + cardsPerPage
 
-	const posts = await getPostsByLimit(client, startLimit, cardsPerPage);
-	const totalPosts = await getPosts(client);
+  const posts = await getPostsByLimit(client, startLimit, cardsPerPage)
+  const totalPosts = await getPosts(client)
+  const siteSettings = await getSiteSettings(client)
 
-	const totalPages = Math.ceil(totalPosts.length / cardsPerPage);
+  const totalPages = Math.ceil(totalPosts.length / cardsPerPage)
 
+  const totalPodcasts = await getPodcastsCount(client)
+  const totalWebinars = await getWebinarsCount(client)
+  const totalArticles = await getArticlesCount(client)
+  const totalEbooks = await getEbooksCount(client)
 
-	const totalPodcasts = await getPodcastsCount(client);
-	const totalWebinars = await getWebinarsCount(client);
-	const totalArticles = await getArticlesCount(client);
-	const totalEbooks = await getEbooksCount(client);
-
-	return {
-		props: {
-			posts,
-			tags,
-			totalPages,
-			totalPosts,
-			currentPage: pageNumber,
-			contentCount:{
-				podcasts: totalPodcasts,
-				webinars: totalWebinars,
-				articles: totalArticles,
-				ebooks: totalEbooks
-			}
-		},
-	};
-};
-
-
-export default function ProjectSlugRoute(
-	props: InferGetStaticPropsType<typeof getStaticProps> & { posts: any, totalPages: any, tags: any },
-) {
-	const router = useRouter();
-
-	const { posts, totalPages, tags,contentCount,totalPosts } = props;
-	const totalCount:any = Object.values(contentCount).reduce((acc:any, count) => acc + count, 0);
-
-
-	const baseUrl = useRef(`/${siteConfig.paginationBaseUrls.base}`).current;
-
-	const handlePageChange = (page: number) => {
-	//   if (page === 1) {
-	// 	router.push(baseUrl);
-	//   } else {
-	// 	router.push(`${baseUrl}/page/${page}`);
-	//   }
-	};
-
-
-	return (
-		<>
-		    <BaseUrlProvider baseUrl={baseUrl}>
-
-			<Layout >
-				<ContentHub contentCount={contentCount}/>
-				<TagSelect
-					tags={tags}
-					tagLimit={5}
-					showTags={true}
-				/>
-				<AllcontentSection allItemCount={totalCount}  allContent={posts} />
-				<Pagination
-					totalPages={totalPages}
-					onPageChange={handlePageChange}
-					currentPage={0}
-					enablePageSlug={true}
-				/>
-				<BannerSubscribeSection />
-			</Layout>
-			</BaseUrlProvider>
-
-		</>
-	)
+  return {
+    props: {
+      posts,
+      tags,
+      totalPages,
+      totalPosts,
+      currentPage: pageNumber,
+      contentCount: {
+        podcasts: totalPodcasts,
+        webinars: totalWebinars,
+        articles: totalArticles,
+        ebooks: totalEbooks,
+      },
+      siteSettings: siteSettings,
+    },
+  }
 }
 
+export default function ProjectSlugRoute(
+  props: InferGetStaticPropsType<typeof getStaticProps> & {
+    posts: any
+    totalPages: any
+    tags: any
+  },
+) {
+  const router = useRouter()
+
+  const { posts, totalPages, tags, contentCount, totalPosts, siteSettings } =
+    props
+  const totalCount: any = Object.values(contentCount).reduce(
+    (acc: any, count) => acc + count,
+    0,
+  )
+
+  const baseUrl = useRef(`/${siteConfig.paginationBaseUrls.base}`).current
+
+  const handlePageChange = (page: number) => {
+    //   if (page === 1) {
+    // 	router.push(baseUrl);
+    //   } else {
+    // 	router.push(`${baseUrl}/page/${page}`);
+    //   }
+  }
+  const siteSettingWithImage = siteSettings?.find((e: any) => e?.openGraphImage)
+
+  return (
+    <>
+      <BaseUrlProvider baseUrl={baseUrl}>
+        <Layout>
+          {siteSettingWithImage ? defaultMetaTag(siteSettingWithImage) : <></>}
+          <ContentHub contentCount={contentCount} />
+          <TagSelect tags={tags} tagLimit={5} showTags={true} />
+          <AllcontentSection allItemCount={totalCount} allContent={posts} />
+          <Pagination
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+            currentPage={0}
+            enablePageSlug={true}
+          />
+          <BannerSubscribeSection />
+        </Layout>
+      </BaseUrlProvider>
+    </>
+  )
+}
