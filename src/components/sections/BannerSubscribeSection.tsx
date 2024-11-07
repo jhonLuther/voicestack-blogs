1e87c9549e1a43e2d72040037f283efb1e485eb0
Akash A S <akashas@carestack.com>
2024-11-07 10:26:52 +0000

    Merged in dev (pull request #79)
    Nested link issues sorted and header issue in pagination fixed 🔨
    
    * build error fix
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * build error fixed
    
    * hot fixes in count  in brows page fixed
    
    * ui fixes in hovering cards
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * pagination fixed all place :hammer:
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * issue resolved
    
    * build fixes
    
    * build fixes
    
    * build fixed version 2
    
    * build fixes version 4
    
    * ui fixes build fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * btn hover color changes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * quick  fixex in breadcrumb section
    
    * app script connected
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * nested link issues sorted and header issue in pagination fixed
    
    * conflicts resolved
    
    * subscription fixes

--- src/components/sections/BannerSubscribeSection.tsx
+++ src/components/sections/BannerSubscribeSection.tsx
@@ -10,65 +10,62 @@ interface BannerSubscribeSectionProps {
 }
 function BannerSubscribeSection({ isSmall }: BannerSubscribeSectionProps) {
   const [email, setEmail] = useState('');
-  const [isSubmitted,setIsSubmitted] =  useState(false)
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const response = await fetch('https://script.google.com/a/macros/carestack.com/s/AKfycbxA63dkm1kfBlfARXWyxsvf2qILj-RkeRiRF8HwRUnJrOBv9Q_MakNx4wlfe31V131u/exec', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: email
-          })
-        });
-        const data = await response.json();
-        setIsSubmitted(true)
-        console.log(data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email
+        })
+      });
+      const data = await response.json();
+      setIsSubmitted(true)
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <Section className='justify-center' >
       <Wrapper className={`flex-col`}>
         <div className={`bg-white flex gap-3`}>
           <div className='flex flex-col gap-6 flex-1'>
-
             <div className='flex gap-1 justify-between items-start md:flex-row flex-col'>
-              <H3XL className={`text-zinc-900 md:text-5xl text-2xl font-monrope tracking-[-0.96px] font-manrope font-extrabold`}>Subscribe to<br/> <span className=' bg-gradient-text2 bg-clip-text text-transparent '>Dentistry’s Inner Circle</span></H3XL>
+              <H3XL className={`text-zinc-900 md:text-5xl text-2xl font-monrope tracking-[-0.96px] font-manrope font-extrabold`}>Subscribe to<br /> <span className=' bg-gradient-text2 bg-clip-text text-transparent '>Dentistry’s Inner Circle</span></H3XL>
               <p className="md:text-lg text-base font-medium md:max-w-[392px] md:self-end text-zinc-700">Get the best, coolest, and latest in the dental industry delivered to your inbox each week.</p>
             </div>
 
-            <div className={`flex md:flex-row flex-col gap-3 items-centerjustify-between relative`}>
-            
-              <input id="default-search"
-                type="email"
-                placeholder="Enter your email address"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                className="block flex-1 w-full rounded-[5px] md:rounded-[10px] border py-3 md:py-4 pl-3 md:pl-6 pr-3 md:pr-4 border-zinc-300
+            {!isSubmitted &&
+              <div className={`flex md:flex-row flex-col gap-3 items-centerjustify-between relative`}>
+
+                <input id="default-search"
+                  type="email"
+                  placeholder="Enter your email address"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="block flex-1 w-full rounded-[5px] md:rounded-[10px] border py-3 md:py-4 pl-3 md:pl-6 pr-3 md:pr-4 border-zinc-300
                  text-zinc-400 font-medium text-sm md:text-2xl h-[53px] md:h-[84px] bg-transparent focus:ring-blue-500 focus:border-blue-500 
                  dark:bg-transparent focus:outline-none dark:placeholder-zinc-400 dark:text-zinc-600 dark:focus:border-blue-500 
                  placeholder-zinc-300"
-                required />
+                  required />
 
-              <Button className='bg-zinc-900 !px-12 hover:bg-zinc-700 md:absolute md:translate-y-[-50%] top-[50%] right-[16px] self-start'  onClick={handleSubmit}>
-                <span className='text-base font-medium'>{`Submit`}</span>
-              </Button>
-              {
-                isSubmitted && <p className="text-xs md:text-base font-medium text-zinc-500 leading-[1.2]">Thank you for subscribing!</p>
-              }
-            </div>
-
+                <Button className='bg-zinc-900 !px-12 hover:bg-zinc-700 md:absolute md:translate-y-[-50%] top-[50%] right-[16px] self-start' onClick={handleSubmit}>
+                  <span className='text-base font-medium'>{`Submit`}</span>
+                </Button>
+              </div>}
+            {
+              isSubmitted && <p className="text-xs md:text-base font-bold text-zinc-500 leading-[1.2]">{`Thank you for subscribing!`}</p>
+            }
           </div>
         </div>
-
         <p className="mt-3 md:mt-6 text-xs md:text-base font-medium text-zinc-500 leading-[1.2]">You can unsubscribe at any time, no hard feelings. <Link href="https://carestack.com/legal/2024-1/privacypolicy" target='_blank' className='underline underline-offset-2'>{`Privacy policy.`} </Link></p>
-
       </Wrapper>
     </Section>
   );
