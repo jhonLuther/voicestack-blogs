00b8340d20daf8be04a9cb9f4cfa0a83243d39c4
Akash A S <akashas@carestack.com>
2025-02-07 06:09:46 +0000

    Merged in dev (pull request #130)
    Tag ui fixes
    
    * testimonial author url fixed
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * jsonld update
    
    * Merge branch 'dev' of httpt pull upstream mains://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * url fixes 🛠️
    
    * url fixed
    
    * Performance improved in browse topics and pagination fixed slow loading issue in low end devices fixes 🛠️
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * ui fixes in toc
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * logo name corrections ✨
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * event card fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * podcast controlled query fixes in index page ⚒️
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * quick fixes ⚒️
    
    * m fixes
    
    * robots changed
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev
    
    * mobile fixes for tags
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/carestack-website-resources into dev

--- src/components/commonSections/RelatedTag.tsx
+++ src/components/commonSections/RelatedTag.tsx
@@ -20,7 +20,7 @@ const RelatedTag = ({ tags, className }: RelatedTagProps) => {
 
   return (
     <Section className={`justify-center border-t border-zinc-200  !pb-0 md:!mt-4  ${className}`}>
-      <div className='w-full  md:flex-row flex flex-col lg:flex-wrap flex-nowrap  gap-4'>
+      <div className='w-full  md:flex-row flex flex-col flex-wrap gap-4'>
         <DescriptionText className='text-zinc-900 font-medium flex  items-center pr-[14px]'>{`Related Tags`}</DescriptionText>
         {tags && tags.length > 0 && tags.map((tag: any) => {
           // const hrefLink = `/${prefLocale}/${siteConfig.paginationBaseUrls.base}/${tag?.slug?.current ? tag?.slug?.current : ''}`cons
@@ -29,7 +29,7 @@ const RelatedTag = ({ tags, className }: RelatedTagProps) => {
           return (
             <Link className='rounded-md bg-zinc-100 px-4 py-[5px] hover:bg-zinc-200 transition-all duration-300 ease-in-out' 
             href={hrefLink} key={tag?.slug?.current} >
-              <DescriptionText className='text-zinc-600 font-normal'>
+              <DescriptionText className='text-zinc-600 font-normal md:text-base text-sm'>
               {tag.tagName}
               </DescriptionText>
               </Link>
